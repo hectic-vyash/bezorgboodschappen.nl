350175ce00af89cfe75d1e06c5c61583ab890740
unknown <vyashbhawan@gmail.com>
2020-04-27 01:03:34 +0200

    added sass styling + auth system

---
+++ resources/views/bb/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('page-title', 'Home')
+
+@section('content')
+<div class="hero-image d-flex justify-content-center">
+    <img src="img/home-image.svg" alt="vrouw die naar een mobiel met product plaatjes kijkt.">
+</div>
+<div class="row vragen align-center">
+    <div class="col-xs-12">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quos nulla aspernatur tempore
+            minus, labore commodi temporibus nobis numquam aperiam id. Eum, vero mollitia! Nihil amet repellat molestias
+            accusantium odit!</p>
+
+    </div>
+</div>
+<div class="row align-center">
+    <div class="col-xs-12">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quos nulla aspernatur tempore
+            minus, labore commodi temporibus nobis numquam aperiam id. Eum, vero mollitia! Nihil amet repellat molestias
+            accusantium odit!</p>
+
+    </div>
+</div>
+@endsection
